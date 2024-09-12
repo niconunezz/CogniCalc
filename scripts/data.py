37bb9c3cb855b751d13181ccca54534c0a44ce0d
niconunezz <niconuwii@gmail.com>
2024-09-12 23:10:07 +0200

    optimized a little and adding integers to our domain

--- scripts/data.py
+++ scripts/data.py
@@ -3,35 +3,43 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 def get_int(t):
-    nums = [str(int(i.item())) for i in t]
+    nums = [str(int(i.item())) for i in t[1:]]
     num = int(''.join(nums))
-    return num
+   
+    return -num if t[0] == 13 else num
 
-def split_int(num):
-    return [int(i) for i in str(num)]
+def split_int(num: int):
+    return [13 if str(num)[0] == '-' else 14] + [int(i) for i in str(num)[1:]]
 
-def add_zeros(num: list, sze):
-    while len(num) < sze:
-        num = [0] + num
-    return num
+def add_zeros(num: list, sze: int):
+    return [num[0]] + [0]*(sze - len(num)) + num[1:]
+
+
+def create_addend(batch_size, digits):
+    sign = torch.randint(13, 15, (batch_size, 1))
+    addend = torch.randint(0, 9, (batch_size, digits))
+
+    return torch.column_stack([sign, addend])
 
 def preprocessed_batch(batch_size, digits):
-    a = torch.randint(0, 9, (batch_size, digits))
-    b = torch.randint(0, 9, (batch_size, digits))
+    a = create_addend(batch_size, digits)
+    b = create_addend(batch_size, digits)
     a = torch.column_stack([a, torch.full((batch_size,), 10)])
     sm = torch.cat([a, b], dim= 1)
     sm = torch.column_stack([sm, torch.full((batch_size,), 11)])
     sums = []
     for t in sm:
         first_half = get_int(t[:digits])
-        second_half = get_int(t[digits+ 1:-1])
+        second_half = get_int(t[digits+ 2:-1])
         sums.append(first_half + second_half)
         
     y = []
     for number in sums:
         splitednum = split_int(number)
-        if len(splitednum) < (digits + 1):
-            splitednum = add_zeros(splitednum, digits + 1)
+        if len(splitednum) < (digits + 2):
+            splitednum = add_zeros(splitednum, digits + 2)
+        
+        print(f"Splited: {splitednum}")
         y.append(splitednum)
         
     y = torch.tensor([i[::-1] for i in y])
@@ -42,7 +50,6 @@ def preprocessed_batch(batch_size, digits):
 def get_val_data(samples: int, digits: int):
     
     x,y = preprocessed_batch(samples, digits)
-
     
     assert x.shape == y.shape, f"Shapes do not match: {x.shape} != {y.shape}"
     return x.long(),y.long()
@@ -64,14 +71,14 @@ def get_batch(batch_size, digits, specials):
     
     # we ensure the model has never seen some numbers during training
     while not g:
-        
         x, y = preprocessed_batch(batch_size, digits)
-
         # returns false if any tensor of x is in specials
+        print(f"x {x}")
+        print(f"y {y}")
         g = discriminate(x, specials)
 
-    
-
     assert x.shape == y.shape, f"Shapes do not match: {x.shape} != {y.shape}"
-    return x.long(),y.long()
+    return x.long(), y.long()
 
+
+print(get_batch(1, 5, torch.randint(0, 15, (1, (4 * 5 + 5)))))
