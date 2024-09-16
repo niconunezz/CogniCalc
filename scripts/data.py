394fb9e91878eff240d81a6584e8b6c0da43cbde
niconunezz <niconuwii@gmail.com>
2024-09-16 21:18:47 +0200

    Revert "fixed important bugs"
    This reverts commit 53ef07fb7e5e3a6b3848baac48b71189a861cc36.

--- scripts/data.py
+++ scripts/data.py
@@ -6,14 +6,10 @@ def get_int(t):
     nums = [str(int(i.item())) for i in t[1:]]
     num = int(''.join(nums))
    
-    return (-1)*num if t[0] == 13 else num
+    return -num if t[0] == 13 else num
 
 def split_int(num: int):
-    if str(num)[0] == "-":
-        splited = [13] + [int(i) for i in str(num)[1:]]
-    else:
-        splited = [14] + [int(i) for i in str(num)]
-    return splited
+    return [13 if str(num)[0] == '-' else 14] + [int(i) for i in str(num)[1:]]
 
 def add_zeros(num: list, sze: int):
     return [num[0]] + [0]*(sze - len(num)) + num[1:]
@@ -33,17 +29,15 @@ def preprocessed_batch(batch_size, digits):
     sm = torch.column_stack([sm, torch.full((batch_size,), 11)])
     sums = []
     for t in sm:
-        first_half = get_int(t[:digits+1])
+        first_half = get_int(t[:digits])
         second_half = get_int(t[digits+ 2:-1])
         sums.append(first_half + second_half)
         
     y = []
     for number in sums:
-
         splitednum = split_int(number)
         if len(splitednum) < (digits + 2):
             splitednum = add_zeros(splitednum, digits + 2)
-        # print(f"Number: {number} Splited: {splitednum}")
         y.append(splitednum)
         
     y = torch.tensor([i[::-1] for i in y])
@@ -82,5 +76,3 @@ def get_batch(batch_size, digits, specials):
     assert x.shape == y.shape, f"Shapes do not match: {x.shape} != {y.shape}"
     return x.long(), y.long()
 
-
-# print(get_batch(1, 5, torch.randint(0, 15, (1, (4 * 5)))))
