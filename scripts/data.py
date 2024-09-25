38e0641e4ea144d08b1242c0a53fd62226348af8
niconunezz <niconuwii@gmail.com>
2024-09-25 21:16:09 +0200

    grouped into a class

--- scripts/data.py
+++ scripts/data.py
@@ -2,67 +2,80 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def get_int(tensor):
-    nums = [int(''.join(map(str, row.tolist()))) for row in tensor]
-    return nums
-
-def split_int(num: int) -> list:
-    return [int(i) for i in str(num)]
-
-def add_zeros(num: list, sze: int) -> list:
-    return [0]*(sze - len(num)) + num
-
-def preprocessed_batch(batch_size: int, digits: int):
+class DataLoaderLite:
+    def __init__(self, batch_size, digits, samples, discriminate_var = False):
+        self.batch_size = batch_size
+        self.digits = digits
+        self.samples = samples
+        specials, specials_labels = self.get_val_data()
+        self.specials = specials
+        self.specials_labels = specials_labels
+        self.discriminate_var = discriminate_var
     
-    ab = torch.randint(0, 9, (batch_size, digits*2))
-    a, b = torch.split(ab, digits, dim= 1)
+    def get_int(self, tensor) -> list[int]:
+        nums = [int(''.join(map(str, row.tolist()))) for row in tensor]
+        return nums
 
-    a = torch.column_stack([a, torch.full((batch_size,), 10)])
-    sm = torch.column_stack([a, b])
-    sm = torch.column_stack([sm, torch.full((batch_size,), 11)])
+    def split_int(self, num: int) -> list[int]:
+        return [int(i) for i in str(num)]
+
+    def add_zeros(self, num: list, sze: int) -> list[int]:
+        return [0]*(sze - len(num)) + num
     
-    first_half = get_int(sm[: , :digits])
-    second_half = get_int(sm[: , digits+ 1:-1])
-    sums = [i + j for i, j in zip(first_half, second_half)]
+    def preprocessed_batch(self, val=False) -> tuple[torch.Tensor, torch.Tensor]:
+        
+        if val:
+            B = self.samples
+        else:
+            B = self.batch_size
 
-    y = [add_zeros(split_int(number), digits + 1) for number in sums]
+        ab = torch.randint(0, 9, (B, self.digits*2))
+        a, b = torch.split(ab, self.digits, dim= 1)
+
+        a = torch.column_stack([a, torch.full((B,), 10)])
+        sm = torch.column_stack([a, b])
+        sm = torch.column_stack([sm, torch.full((B,), 11)])
+        
+        first_half = self.get_int(sm[: , :self.digits])
+        second_half = self.get_int(sm[: , self.digits+ 1:-1])
+        sums = [i + j for i, j in zip(first_half, second_half)]
+
+        y = [self.add_zeros(self.split_int(number), self.digits + 1) for number in sums]
+        
+        y = torch.tensor([i[::-1] for i in y])
+        x = torch.column_stack([sm, y[:, :self.digits]])
+        y = torch.column_stack([torch.full((B, x.shape[1] - self.digits- 1), 12), y]) # padding
+
+        assert x.shape == y.shape, f"Shapes do not match: {x.shape} != {y.shape}"
+        return x, y
     
-    y = torch.tensor([i[::-1] for i in y])
-    x = torch.column_stack([sm, y[:, :digits]])
-    y = torch.column_stack([torch.full((batch_size,x.shape[1] - digits- 1), 12), y]) # padding
-    return x, y
-
-def get_val_data(samples: int, digits: int):
     
-    x,y = preprocessed_batch(samples, digits)
+    def get_val_data(self) -> tuple[torch.Tensor, torch.Tensor]:
+        x,y = self.preprocessed_batch(val=True)        
+        return x.long(), y.long()
 
-    assert x.shape == y.shape, f"Shapes do not match: {x.shape} != {y.shape}"
-    return x.long(), y.long()
-
-def discriminate(t, specials):
-    for i in t:
-        for special in specials:
-            if torch.all(torch.eq(i, special)):
-                print(f"Special batch found: {i}")
-                print("="*50)
-                return False
+    def discriminate(self, t: torch.Tensor) -> bool:
+        for i in t:
+            for special in self.pecials:
+                if torch.all(torch.eq(i, special)):
+                    print(f"Special batch found: {i}")
+                    print("="*50)
+                    return False
+        
+        return True
     
-    return True
+    def get_batch(self):
 
+        # if you want to prohibit the model to see val_data during training
+        # spoiler: probability does it by itself
 
-def get_batch(batch_size, digits, specials):
-
-    x, y = preprocessed_batch(batch_size, digits)
-
-    # if you want to prohibit the model to see val_data uncomment
-    # spoiler: probability does it by itself
-
-    # g = False
-    # while not g:
-    #     x, y = preprocessed_batch(batch_size, digits)
-    #     # returns false if any tensor of x is in specials
-    #     g = discriminate(x, specials)
-
-    
-    assert x.shape == y.shape, f"Shapes do not match: {x.shape} != {y.shape}"
-    return x.long(),y.long()
+        if self.discriminate_var:
+            g = False
+            while not g:
+                x, y = self.preprocessed_batch()
+                # returns false if any tensor of x is in specials
+                g = self.discriminate(x)
+        else:
+            x, y = self.preprocessed_batch()
+        
+        return x.long(),y.long()
